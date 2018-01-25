8f74abf73c5624dd9c7f7bdbc69afbb5b8578a89
k4ani5h <gargkanish23@gmail.com>
2018-01-25 18:49:15 +0000

    speech to text

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 import pyaudio
 import numpy as np
 
-CHUNK = 4096 # number of data points to read at a time
+CHUNK = 32 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
 
 p=pyaudio.PyAudio() # start the PyAudio class
@@ -9,7 +9,7 @@ stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK) #uses default input device
 
 # create a numpy array holding a single read of audio data
-for i in range(1000): #to it a few times just to see
+for i in range(50): #to it a few times just to see
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
     print(data)
 
