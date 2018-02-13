aaf1bd33ab89199a2070282787b8b9bc649c9a0a
Kanish <gargkansih23@gmail.com>
2018-02-13 19:44:10 +0000

    add recognition with emotions

--- first.py
+++
@@ -1,6 +0,0 @@
-import sounddevice #pip install sounddevice
-
-for i in range(30): #30 updates in 1 second
-    rec = sounddevice.rec(44100/30)
-    sounddevice.wait()
-    print(rec.shape)
--- speech_recognition/__main__.py
+++ speech_recognition/__main__.py
@@ -1,5 +1,32 @@
 import speech_recognition as sr
 
+import requests
+import json
+
+def analyze_tone(text):
+    username = '926325e3-3eea-4494-86c0-c03d1a9deefe'
+    password = 'CuExNGBJgChT'
+    watsonUrl = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-2'
+    headers = {"content-type": "text/plain"}
+    data = text
+    try:
+        r = requests.post(watsonUrl, auth=(username,password), headers = headers,
+         data=data)
+        return r.text
+    except:
+        return False
+
+def display_results(data):
+    if len(data) >= 1:
+        if data == 'q'.lower():
+            exit
+        results = analyze_tone(data)
+        if results != False:
+            results = json.loads(str(results))
+            return results['document_tone']['tone_categories']
+        else:
+            return "Something went wrong"
+
 r = sr.Recognizer()
 m = sr.Microphone()
 
@@ -17,9 +44,11 @@ try:
 
             # we need some special handling here to correctly print unicode characters to standard output
             if str is bytes: # this version of Python uses bytes for strings (Python 2)
-                print(u"You said {}".format(value).encode("utf-8"))
+                print(u"You say {}".format(value).encode("utf-8"))
+                print(display_results(format(value).encode("utf-8")))
             else: # this version of Python uses unicode for strings (Python 3+)
-                print("You said {}".format(value))
+                print("You say {}".format(value))
+                print(display_results(format(value)))
         except sr.UnknownValueError:
             print("Oops! Didn't catch that")
         except sr.RequestError as e:
