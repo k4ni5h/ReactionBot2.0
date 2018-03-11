948ad448bc74db7db3b66ae24a75cead6dd91b69
Kanish <gargkanish23@gmail.com>
2018-03-11 16:18:35 +0530

    sound with less error

--- rec.py
+++ rec.py
@@ -1,28 +1,54 @@
 import pyaudio
-import wave
-import sys
+import numpy as np
+import time
+import math
+import threading
+'''from multiprocessing import Process
 
-chunk = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+def runInParallel(*fns):
+    proc = []
+    for fn in fns:
+        p = Process(target=fn)
+        p.start()
+        proc.append(p)
+    for p in proc:
+        p.join()
+# don't use scientific notation
+'''
+CHUNK = 2048 # number of data points to read at a time
+RATE = 44100 # time resolution of the recording device (Hz)
+d1=0
+d2=0
+def func(i):
+    d=0
+    t=time.time()
+    p=pyaudio.PyAudio() # start the PyAudio class
+    stream=p.open(format=pyaudio.paInt16,
+                  channels=1,
+                  rate=RATE,
+                  input=True,
+                  input_device_index = i,
+                  frames_per_buffer=CHUNK) #uses default input device
+    while(True): #to it a few times just to see
+        c=time.time()
+        data = np.fromstring(stream.read(CHUNK,exception_on_overflow = False),
+                             dtype=np.int32)
+        data = np.abs(data)
+        print(i, " : " ,np.argmax(data))
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-p = pyaudio.PyAudio()
+try:
+    t1 = threading.Thread(target=func, args = (3,)).start()
+    t2 = threading.Thread(target=func, args = (2,)).start()
+    t1.join
+    t2.join
 
-stream = p.open(format = FORMAT,
-                channels = CHANNELS,
-                rate = RATE,
-                input = True,
-                frames_per_buffer = chunk)
+except:
+   print("Error: unable to start thread")
 
-print("* recording")
-all = []
-for i in range(0, int(RATE / chunk * RECORD_SECONDS)):
-    data = stream.read(chunk, exception_on_overflow = False)
-    all.append(data)
-print("* done recording")
+#runInParallel(func(1), func(2))
+#np.set_printoptions(suppress=True)
 
-stream.close()
-p.terminate()
+# close the stream gracefully
